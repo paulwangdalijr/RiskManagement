d34f236d8feeb34d08314fb5b2d04458d3ee5a4f
Paul Jr Wangdali <paulwangdalijr@gmail.com>
2023-12-14 19:23:06 +0000

    Initial Commit

---
+++ app/risks-management/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'risksmanagement/test/integration/FirstJourney',
+		'risksmanagement/test/integration/pages/RisksList',
+		'risksmanagement/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('risksmanagement') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
